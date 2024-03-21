8c39ec4593b388a10852d76d28c7600ceca41cb8
andres.mera <feli041089@gmail.com>
2024-03-21 10:30:20 -0500

    developer : No te repitas a ti mismo - DRY.

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -21,19 +21,23 @@ export class AuthService {
 
   constructor() { }
 
+  private setAuthentication(user: User, token: string): boolean {
+
+    this._currentUser.set(user);
+    this._authStatus.set(AuthStatus.aunthenticated);
+    localStorage.setItem('token', token);
+
+    return true;
+  }
+
   login(email: string, password: string): Observable<boolean> {
 
     const url: string = `${this.baseUrl}/auth/login`;
     const body = { email, password };
     return this.http.post<LoginResponse>(url, body)
       .pipe(
-        tap(({ user, token }) => {
-          this._currentUser.set(user);
-          this._authStatus.set(AuthStatus.aunthenticated);
-          localStorage.setItem('token', token);
-        }),
-        map(() => true),
-        catchError(err => throwError(() => err.error.message))
+        map(({ user, token }) => this.setAuthentication(user, token)),
+        catchError(err => throwError(() => err.error.message)),
       );
   }
 
@@ -49,12 +53,7 @@ export class AuthService {
     return this.http
       .get<CheckTokenResponse>(url, { headers })
       .pipe(
-        map(({ token, user }) => {
-          this._currentUser.set(user);
-          this._authStatus.set(AuthStatus.aunthenticated);
-          localStorage.setItem('token', token);
-          return true;
-        }),
+        map(({ user, token }) => this.setAuthentication(user, token)),
         catchError(() => {
           this._authStatus.set(AuthStatus.notAunthenticated);
           return of(false);
