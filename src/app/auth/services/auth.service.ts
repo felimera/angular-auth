623a5a81e99c88400a60404b038146bd3c592e50
andres.mera <feli041089@gmail.com>
2024-03-21 07:39:58 -0500

    developer : AuthService.

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+
+import { environment } from '../../../environments/environments';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  private readonly baseUrl: string = environment.baseUrl;
+  private http = inject(HttpClient);
+
+  private _currentUser = signal<User | null>(null);
+  private _authStatus = signal<AuthStatus>();
+
   constructor() { }
+
+  login(email: string, password: string): Observable<boolean> {
+    return of(true);
+  }
 }
